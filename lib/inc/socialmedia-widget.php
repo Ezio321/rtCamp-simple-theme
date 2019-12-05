4320a6e9c3460f015fafd508731a3a507f55a303
mrugeshdixit <mrugeshmdixit@gmail.com>
2019-12-05 18:11:03 +0530

    Add files via upload

--- lib/inc/socialmedia-widget.php
+++ lib/inc/socialmedia-widget.php
@@ -77,11 +77,7 @@ class News_widget extends WP_Widget {
       $number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
       $show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
       ?>
-      <p><label for="<?php echo $this->get_field_id( 'title' ); ?>">
-              <?php _e( 'Title:' ); ?></label>
-          <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
-      
-      <p><label for="<?php echo $this->get_field_id( 'number' ); ?>">
+         <p><label for="<?php echo $this->get_field_id( 'number' ); ?>">
               <?php _e( 'Number of posts to show:'); ?></label>
           <input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" /></p>
       
@@ -188,11 +184,7 @@ add_action( 'widgets_init', function() {
 
         
 
-        <p>
-          <label for="<?php echo $this->get_field_id('title'); ?>">
-              <?php _e('Title:', 'socialmedia'); ?></label>
-          <input class="Title" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo ($title); ?>">
-        </p>
+        
 
         <p>
           <label for="<?php echo $this->get_field_id('facebook'); ?>">
